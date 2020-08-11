20fa43288eb1b87d3897954f0bb271564147c631
Brian Warner <brw1820@gmail.com>
2020-08-10 23:18:46 -0400

    Updates

---
+++ precache-manifest.271b9e0c8dd5be7a3d1088f988e0c14f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb84a3b754e4a7239ca80254e3a6864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1cc1251e6f7d498e2f5",
+    "url": "/static/css/main.b88f5255.chunk.css"
+  },
+  {
+    "revision": "33ee7f1f6069ef9e48ba",
+    "url": "/static/js/2.3b1276a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b1276a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cc1251e6f7d498e2f5",
+    "url": "/static/js/main.f6d0471b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c1f3251d796b1947c2af4104d8a19a09",
+    "url": "/static/media/background.c1f3251d.jpg"
+  },
+  {
+    "revision": "25021d63211b390e7dd3ea7864419f52",
+    "url": "/static/media/linkedinpicture.25021d63.png"
+  },
+  {
+    "revision": "71d66150144792f461daffea0df6b18a",
+    "url": "/static/media/resume.71d66150.PDF"
+  }
+]);
