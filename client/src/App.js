9765ae9c76e5dd4bd62031cc6529654f29100c37
Brian Warner <brw1820@gmail.com>
2020-08-10 20:44:27 -0400

    added node version to package.json

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React, {useEffect} from 'react';
 // import SiteContainer from "./components/SiteContainer";
 import axios from "axios";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from "./pages/Home.jsx.js";
-import Contact from './pages/Contact.jsx.js';
-import Resume from './pages/Resume.jsx.js';
+import Home from "./pages/Home.jsx.js.js";
+import Contact from './pages/Contact.jsx.js.js';
+import Resume from './pages/Resume.jsx.js.js';
 import "./pages/Assets/style.css";
-import Nav from "./components/Nav.jsx.js";
-import Projects from "./pages/Projects.jsx.js"
+import Nav from "./components/Nav.jsx.js.js";
+import Projects from "./pages/Projects.jsx.js.js"
 
 // const App = () => <SiteContainer />;
 
